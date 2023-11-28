19f0fb9141d381ec8daeb3da0565a129dc4509b7
MolinQ <kirillaidr@gmail.com>
2023-11-28 18:07:32 +0200

    workDone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PasswordFormComponent } from './password-form/password-form.component';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,9 @@ import { PasswordFormComponent } from './password-form/password-form.component';
     PasswordFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/password-form/password-form.component.ts
+++ src/app/password-form/password-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms'
 
 @Component({
   selector: 'app-password-form',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./password-form.component.scss']
 })
 export class PasswordFormComponent {
+ 
+  form: FormGroup
+  circle1: string = 'gray';
+  circle2: string = 'gray';
+  circle3: string = 'gray';
+  Letters = /[a-zA-Z]/;
+  Numbers = /\d/;
+  Symbols = /[!@#$%^&*(),.?":{}|<>]/;
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      password:['']
+    })
+  }
+
+  ngOnInit() {
+    this.form.get('password')?.valueChanges.subscribe(value => {
+      const hasLetters = this.Letters.test(value);
+      const hasNumbers = this.Numbers.test(value);
+      const hasSymbols = this.Symbols.test(value);
+
+      if (value.length === 0) {
+        this.circle1 = this.circle2 = this.circle3 = 'gray';
+      } else if (value.length < 8) {
+        this.circle1 = this.circle2 = this.circle3 = 'red';
+      } else if (hasLetters && hasNumbers && hasSymbols) {
+        this.circle1 = this.circle2 = this.circle3 = 'green';
+      } else if ((hasLetters && hasNumbers) || (hasLetters && hasSymbols) || (hasNumbers && hasSymbols)) {
+        this.circle1 = this.circle2 = 'yellow';
+        this.circle3 = 'gray';
+      } else {
+        this.circle1 ='red'; this.circle2 = this.circle3 = 'gray';
+      }
+    });
+  }
 }
